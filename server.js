0b57e0da92079bbba8d39a482b7ed5b7a2e2df3d
Alex Norton <atn362@gmail.com>
2021-04-26 16:32:43 -0500

    comments added

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 
+//const to utilize the express app and send app to the local host at a desired port
 const app = express();
 var PORT = process.env.PORT || 3000;
 
+//add encoded into readable source, and converting into json
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,7 +13,7 @@ app.use(express.static("public"));
 require("./apiRoutes")(app);
 require("./htmlRoutes")(app);
 
-
+//listening on the specified port above
 app.listen(PORT, function () {
     console.log(`App listening on http://localhost:${PORT}`);
 });
